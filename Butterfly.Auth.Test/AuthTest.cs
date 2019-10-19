237e3bbce8f268f0ece0f29d2292e2d6fb025987
Kent Johnson <kent@fireshark.com>
2019-10-19 09:42:17 -0500

    Changes

--- Butterfly.Auth.Test/AuthTest.cs
+++ Butterfly.Auth.Test/AuthTest.cs
@@ -18,7 +18,7 @@ namespace Butterfly.Core.Test {
         protected static readonly Logger logger = LogManager.GetCurrentClassLogger();
 
         [TestMethod]
-        public async Task SimpleTest() {
+        public async Task RegistrationTests() {
             var database = new Butterfly.Db.Memory.MemoryDatabase();
 
             await database.CreateFromResourceFileAsync(Assembly.GetExecutingAssembly(), "Butterfly.Auth.Test.db.sql");
@@ -28,10 +28,10 @@ namespace Butterfly.Core.Test {
             database.AddInputPreprocessor(BaseDatabase.RemapTypeInputPreprocessor<DateTime>(dateTime => dateTime.ToUnixTimestamp()));
 
             // Create a single instance of AuthManager
-            AuthManager authManager = new AuthManager(database, userTableRoleFieldName: null);
+            AuthManager authManager = new AuthManager(database);
 
-            // Register a user
-            UserRefToken registerAuthToken = await authManager.RegisterAsync(new {
+            // Register a valid user
+            UserRefToken registerAuthToken1 = await authManager.RegisterAsync(new {
                 username = "johnsmith",
                 first_name = "John",
                 last_name = "Smith",
@@ -39,11 +39,76 @@ namespace Butterfly.Core.Test {
                 phone = "+13162105368",
                 password = "test123"
             });
-            //if (database.CanJoin) {
-                AuthToken authToken = await authManager.AuthenticateAsync(UserRefTokenAuthenticator.AUTH_TYPE, registerAuthToken.id);
-            //}
 
-            await authManager.ForgotPasswordAsync("johnsmith");
+            // Verify the returned auth token can be authenticated
+            var authToken1 = await authManager.AuthenticateAsync(UserRefTokenAuthenticator.AUTH_TYPE, registerAuthToken1.id);
+
+            // Register an invalid email
+            Exception exception2 = null;
+            try {
+                UserRefToken registerAuthToken2 = await authManager.RegisterAsync(new {
+                    username = "johnsmith2",
+                    first_name = "John",
+                    last_name = "Smith",
+                    email = "john",
+                    phone = "+13162105368",
+                    password = "test123"
+                });
+            }
+            catch (Exception e) {
+                exception2 = e;
+            }
+            Assert.IsTrue(exception2.Message.Contains("Email address must contain"));
+
+            // Register an invalid phone
+            Exception exception3 = null;
+            try {
+                UserRefToken registerAuthToken2 = await authManager.RegisterAsync(new {
+                    username = "johnsmith2",
+                    first_name = "John",
+                    last_name = "Smith",
+                    email = "john@fireshark.com",
+                    phone = "123",
+                    password = "test123"
+                });
+            }
+            catch (Exception e) {
+                exception3 = e;
+            }
+            Assert.IsTrue(exception3.Message.Contains("Invalid phone number"));
+
+            // Register a duplicate username
+            Exception exception4 = null;
+            try {
+                UserRefToken registerAuthToken2 = await authManager.RegisterAsync(new {
+                    username = "johnsmith",
+                    first_name = "John",
+                    last_name = "Smith",
+                    email = "john@fireshark.com",
+                    phone = "+13162105368",
+                    password = "test123"
+                });
+            }
+            catch (Exception e) {
+                exception4 = e;
+            }
+            Assert.IsTrue(exception4.Message.Contains("unavailable"));
+
+            // Register without a password
+            Exception exception5 = null;
+            try {
+                UserRefToken registerAuthToken2 = await authManager.RegisterAsync(new {
+                    username = "johnsmith2",
+                    first_name = "John",
+                    last_name = "Smith",
+                    email = "john@fireshark.com",
+                    phone = "+13162105368"
+                });
+            }
+            catch (Exception e) {
+                exception5 = e;
+            }
+            Assert.IsTrue(exception5.Message.Contains("password cannot be null"));
         }
 
     }
--- README.md
+++ README.md
@@ -14,13 +14,76 @@
 
 # Getting Started
 
+## Creating an AuthManager instance
+
+Normally, you will create a single instance of *AuthManager*.  
+*AuthManager* only requires passing in an *IDatabase* instance; 
+however, the following pattern is useful to get an *AuthManager*
+that verifies emails, verifies phone numbers, sends welcome emails,
+sends forgot password emails, etc.
+
+```cs
+var database = (initialize an IDatabase instance here)
+var sendMessageQueueManager = (initialize SendMessageQueueManager here)
+var welcomeEmailSendMessage = (load welcome email here)
+var resetEmailSendMessage = (load reset email here)
+var authManager = new AuthManager(
+    database,
+    onEmailVerify: sendMessageQueueManager.VerifyAsync,
+    onPhoneVerify: sendMessageQueueManager.VerifyAsync,
+    onRegister: user => {
+        sendMessageQueueManager.Queue(welcomeEmailSendMessage.Evaluate(user));
+    },
+    onForgotPassword: user => {
+        sendMessageQueueManager.Queue(resetEmailSendMessage.Evaluate(user));
+    }
+);
+```
+
 ## Database Structure
 
-Butterfly.Auth requires [Butterfly.Db](https://github.com/firesharkstudios/butterfly-db) to manage authentication tokens, users, and accounts.
+Butterfly.Auth requires [Butterfly.Db](https://github.com/firesharkstudios/butterfly-db) 
+to manage authentication tokens, users, and accounts.
 
-While you can use any database engine supported by [Butterfly.Db](https://github.com/firesharkstudios/butterfly-db), here is the SQL to create the necessary tables in MySQL...
+While you can use any database engine supported by [Butterfly.Db](https://github.com/firesharkstudios/butterfly-db), 
+here is the SQL to create the necessary tables in MySQL...
 
+```
+CREATE TABLE account (
+    id VARCHAR(50) NOT NULL,
+    created_at INT NOT NULL,
+    updated_at INT NOT NULL,
+    PRIMARY KEY(id)
+);
 
+CREATE TABLE user(
+    id VARCHAR(50) NOT NULL,
+    account_id VARCHAR(50) NOT NULL,
+    username VARCHAR(40) NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    email_verified_at INT NULL,
+    phone VARCHAR(20) NULL,
+    phone_verified_at INT NULL,
+    salt VARCHAR(40) NOT NULL,
+    password_hash VARCHAR(90) NOT NULL,
+    reset_code VARCHAR(6) NULL,	
+    reset_code_expires_at INT NULL,	
+    created_at INT NOT NULL,
+    updated_at INT NOT NULL,
+    PRIMARY KEY(id),
+    UNIQUE INDEX username(username)
+);
+
+CREATE TABLE auth_token(
+    id VARCHAR(50) NOT NULL,
+    user_id VARCHAR(50) NOT NULL,
+    expires_at INT NOT NULL,
+    created_at INT NOT NULL,
+    PRIMARY KEY(id)
+);
+```
 
 # Contributing
 
